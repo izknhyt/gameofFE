edfb724f2f677c00519cc4eb71ceac35e0d4e01d
izknhyt <nemutesu@gmail.com>
2025-06-22 21:46:04 +0900

    Rename project to gameoffe

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gemeoffe/main.dart';
+import 'package:gameoffe/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
